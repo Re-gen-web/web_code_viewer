9efb6a028b829eb0b162a201f38952efbf84c14a
Benjamin <124823670+lolmemehaha@users.noreply.github.com>
2023-05-01 11:23:49 -0500

    finished

--- open_me.py
+++ open_me.py
@@ -1,1 +1,5 @@
-import requests
+import requests import time
+webpage = input("what is the webpage link? (include http://) ")
+x = requests.get(webpage, verify=False)
+print(x.text) #will return the HTML of the website
+time.sleep(999999999999999)
